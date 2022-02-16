2becf031c78f703fa46b6daca0d21463a867129f
JoshSalame <josh.salame@perficient.com>
2022-02-16 15:51:03 -0500

    Initial commit. Used Spring Initializr. Needs database configuration to run.

---
+++ appt-mgmt-sys-jjc/src/main/java/com/jjcperf/apptmgmtsysjjc/ApptMgmtSysJjcApplication.java
@@ -0,0 +1,13 @@
+package com.jjcperf.apptmgmtsysjjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptMgmtSysJjcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApptMgmtSysJjcApplication.class, args);
+    }
+
+}
---
+++ appt-mgmt-sys-jjc/src/test/java/com/jjcperf/apptmgmtsysjjc/ApptMgmtSysJjcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjcperf.apptmgmtsysjjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApptMgmtSysJjcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
