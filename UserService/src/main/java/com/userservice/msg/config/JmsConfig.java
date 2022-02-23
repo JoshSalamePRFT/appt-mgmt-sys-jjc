8479bf8239a64bd1ed14824989408d733c02980f
JoshSalame <josh.salame@perficient.com>
2022-02-23 13:40:37 -0500

    Refactored package from com.example.userservice to com.userservice. Added many to many relationship in User and Appointment tables. Copied over model files to all services, and ran each, still working.

--- UserService/src/main/java/com/userservice/msg/config/JmsConfig.java
+++ UserService/src/main/java/com/userservice/msg/config/JmsConfig.java
@@ -1,4 +1,4 @@
-package com.example.userservice.msg.config;
+/*package com.example.userservice.msg.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,4 +19,4 @@ public class JmsConfig {
         converter.setTypeIdPropertyName("_type");
         return converter;
     }
-}
+}*/
