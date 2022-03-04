2830f84a300308fd44ebd0f9e80505625114d395
JoshSalame <josh.salame@perficient.com>
2022-03-04 02:26:05 -0500

    Added "Add Appt" page and functionality. Added delete appt functionality in the View All Appts page. Refactored some of obj.js to separate out any shared action flags. General page cleanup (removing unnecessary buttons, adding classes to tables for styling purposes, etc).

--- AppointmentManagementService/src/main/resources/static/scripts/manage-appointments.js
+++ AppointmentManagementService/src/main/resources/static/scripts/manage-appointments.js
@@ -9,25 +9,15 @@ $(document).ready(function () {
         for (var i = 0; i < json.length; i++) {
             let appt = new Appointment(json[i]);
             // console.log(appt.toJSON());
-            $table.append(appt.toTableRow(true));
+            $table.append(appt.toTableRow(false, true));
             apptList.push(appt);
         }
         console.log(apptList);
     });
 });
 
-$(document).delegate('.edit-appt', 'click', function (e) {
-    e.preventDefault();
-    const apptID = e.target.value;
-    const appt = apptList.find<Appointment>(a => a.appointment_id == apptID);
-    const row = document.getElementById(apptID);
-    console.log(`${apptID} :: ${appt} :: ${row}`);
-
-    //TODO remove function and buttons (doesn't make sense to edit entry in join table. Should only have delete/unrsvp.
-});
-
 $(document).delegate('.unrsvp-appt', 'click', function (e) {
-//TODO change mapping to delete from users_appts
+    //TODO change mapping to delete from users_appts
     e.preventDefault();
     let apptID = e.target.value;
     $.ajax({
